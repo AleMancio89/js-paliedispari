0bc695daaef69bcbd43507ecb83702b1b4687a58
Alessandro <alessandro.mancinelli89@gmail.com>
2020-10-27 17:18:14 +0100

    Palindromo done

---
+++ js-palindromo/js/script.js
@@ -0,0 +1,26 @@
+var input = prompt('Inserisci una parola per verificare che sia un palindromo');
+
+//Definisco la funzione per stabilire palindromo true o false
+
+function isPalindrome(input) {
+  var stringLowCase = input.toLowerCase(); // Riduco tutte le lettere in minuscolo per la comparazione
+
+  for (var i = 0; i < stringLowCase.length; i++){
+
+    if (stringLowCase[i] != stringLowCase[stringLowCase.length - 1 - i]) {
+    return false;
+    }
+    
+  }
+
+  return true;
+
+}
+
+//Gestisco l'output tramite un alert in base al true/false
+
+if ( isPalindrome(input) == true){
+  alert('La parola inserita è palindroma');
+} else {
+  alert('La parola inserita NON è palindroma');
+}
