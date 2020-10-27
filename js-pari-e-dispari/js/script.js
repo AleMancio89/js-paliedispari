067c55b3c417f8ceb49ae4d21cfa25e5f0a00d9e
Alessandro <alessandro.mancinelli89@gmail.com>
2020-10-27 18:11:53 +0100

    Palindromo e Pari e dispari done

---
+++ js-pari-e-dispari/js/script.js
@@ -0,0 +1,44 @@
+//Raccolta input
+var inputNumber = parseInt(prompt('Inserisci un numero da 1 a 5'));
+
+var inputEvenOrOdd = (prompt('Scegli se, secondo te, il risultato sarà pari o dispari')).toLowerCase();
+
+var somma = inputNumber;
+
+// Funzione per calcolare numero random
+
+function computerRandomNumber() {
+  var computerRandomNumber = Math.floor(Math.random() * 5 + 1);
+
+  return computerRandomNumber;
+
+}
+
+somma += computerRandomNumber();
+
+//Funzione per stabilire se la somma dei due numeri è pari
+
+function isEven() {
+  var even = false;
+  if (somma % 2 == 0){
+    return true;
+  }
+
+  return even
+}
+
+//Condizioni per stabilire la vittoria
+
+if (isEven(somma)){
+  if (inputEvenOrOdd == 'pari') {
+    alert('Hai Vinto! il numero ' + somma + ' è pari!')
+  } else {
+    alert('Hai Perso! il numero ' + somma + ' è pari!')
+  }
+} else {
+  if (inputEvenOrOdd == 'pari') {
+    alert('Hai Perso! il numero ' + somma + ' è dispari!')
+  } else {
+    alert('Hai Vinto! il numero ' + somma + ' è dispari!')
+  }
+}
